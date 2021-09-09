6e57698f87b8011ace0fbb88cdd439270a869e8c
phydy <mphidel@gmail.com>
2021-09-09 13:38:24 +0300

    modified interct-erc20.py

--- scripts/interact-erc20.py
+++ scripts/interact-erc20.py
@@ -1,6 +1,9 @@
 from brownie import ERC20Basic, config, accounts, network
+from scripts.helpful_scripts import (
+    get_account
+)
  
 def main():
-    account = accounts.add(config["wallets"]["from_key"]) or accounts[0]
+    account = get_account() #accounts.add(config["wallets"]["from_key"]) or accounts[0]
     erc20 = ERC20Basic[-1]
     print("Total Supply is", erc20.totalSupply())
